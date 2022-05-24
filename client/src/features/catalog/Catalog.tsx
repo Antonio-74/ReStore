827f35158fb0c6f0d45e768d2da794fbd4cac42f
JDGROUP\anlopez <anlopez@mb.jdgroup.net>
2022-05-23 17:53:17 -0700

    End of section 5

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from 'react';
+import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 import { Product } from '../../app/models/product';
 import ProductList from './ProductList';
 
 export default function Catalog() {
 
     const [products, setProducts] = useState<Product[]>([]);
-
-    function getProducts () {
-        fetch('http://localhost:5000/api/Products')
-        .then(res => res.json())
-        .then(data => setProducts(data));
-    }
+    const [loading, setLoading] = useState(true);
     
     useEffect(() => {
-        getProducts();
-    }, [])
+        agent.Catalog.list()
+        .then(products => setProducts(products))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <LoadingComponent message='Loading products...' />
 
     return (
         <>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+//const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -20,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
