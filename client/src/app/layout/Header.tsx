6c84936fc56986c54865c66f158012d6a31ebadc
JDGROUP\anlopez <anlopez@mb.jdgroup.net>
2022-05-27 12:29:44 -0700

    End of section 7

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, ListItem, Switch, Toolbar, Typography, List, IconButton, Badge, Box } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
-import { useStoreContext } from "../context/StoreContext";
+import { useAppSelector } from "../store/configureStore";
 
 interface Props {
     darkMode: boolean;
@@ -33,7 +33,7 @@ const navStyles = {
 
 export default function Header({darkMode, handleThemeChange}: Props) {
 
-    const { basket } = useStoreContext();
+    const { basket } = useAppSelector(state => state.basket);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
